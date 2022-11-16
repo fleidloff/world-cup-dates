dbf12bf43fc3d3bbf5418d2389f2eb8fd1951683
Frederik Leidloff <kontakt@frederik-leidloff.de>
2022-11-16 22:22:22 +0100

    some more stuff

--- src/cli.js
+++ src/cli.js
@@ -6,15 +6,18 @@ const DEFAULT_TEAM = "ger";
 
 export async function cli(args) {
   const arg1 = args[2];
+  const arg2 = args[4];
   let team = DEFAULT_TEAM;
+  let optional = false;
 
   if (arg1 === "--team") {
     team = args[3];
   }
 
-  const matches = await fetchMatches(team);
-  const dates = matches.map(({ datetime }) => moment(datetime).format('dddd L, HH:mm'))
-
+  team = team.toUpperCase();
+ 
+    const matches = await fetchMatches(team);
+    const dates = matches.map(({ datetime }) => moment(datetime).format('dddd L, HH:mm'))
     console.log(dates)
 
 }
@@ -25,5 +28,20 @@ async function fetchMatches(team = DEFAULT_TEAM) {
 
       return matches;
 }
+
+async function fetchAllMatches() {
+    const matches = await fetch(`https://worldcupjson.net/matches/?details=false`)
+      .then(response => response.json())
+
+      return matches;
+}
+
+async function fetchTeams() {
+    const teams = await fetch(`https://worldcupjson.net/teams/`)
+      .then(response => response.json())
+
+      return teams;
+}
+
 
 
